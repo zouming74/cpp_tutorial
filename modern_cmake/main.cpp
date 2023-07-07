54a274f9f763b0d82b596d15d15c71e2674a426f
zouming74 <1071203580@qq.com>
2023-07-07 19:52:43 +0800

    fix: include bug
    fix include bug

--- modern_cmake/main.cpp
+++ modern_cmake/main.cpp
@@ -4,7 +4,7 @@
  * target_link_libraries 使得可以像使用系统和第三方库一样
  * 使用子目录中提供的公开头文件。
  */
-#include <answer/answer.hpp>
+#include "answer/include/answer.hpp"
 
 int main(int argc, const char *argv[]) {
     int expected_answer = answer::find_the_ultimate_answer();
